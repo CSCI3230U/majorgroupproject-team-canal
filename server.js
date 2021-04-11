405804eb66891df2998121cd819c353c5b7b2967
Luca Lotito <luca.lotito@ontariotechu.net>
2021-04-11 02:04:49 -0400

    Test branch for experimental processes

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 let express = require('express');
-
-
 let app = express();
+let session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    genid: uuidv4,
+    resave: false,
+    saveUninitialized: false,
+    secret: 'shinzou wo sasageyo why',
+}))
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/newGame', function(request, response) {
+    response.render('newGame', {
+        title: 'New Game',
+        message: 'This is a New Game Page'
+    });
+});
+
+app.get('/login', function(request, response){
+    response.render('login', {
+        title: 'Login Page',
+        errorMessage: ''
+    });
+});
 
 // first page
 app.get('/', function(request, response) {
     response.send('Welcome to our page!');
 });
+
+//Test data
 
 
 app.set('port', process.env.PORT || 4000);
@@ -17,5 +42,4 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), function(){
     //console.log(`listeining for requests on port 3000`);
     console.log(`Listening on port ${app.get('port')}`);
-
 });
