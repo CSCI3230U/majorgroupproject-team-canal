6311dd854a2c2847fbffd00a502fd6fcd1e76c60
Luca Lotito <luca.lotito@ontariotechu.net>
2021-04-17 04:14:59 -0400

    Creation of Database (non operational)

---
+++ public/scripts/databasehandle.js
@@ -0,0 +1,55 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./public/data/users.db', (err) => {
+    if (err) {
+        console.error('Error while connecting to database: ', err);
+    } else {
+        console.log('Connected to or created SQLite database');
+    }
+});
+
+db.serialize(() => {
+    db.run('DROP TABLE users')
+      .run(`CREATE TABLE users(userId INTEGER PRIMARY KEY,
+                                username TEXT,
+                                password TEXT,
+                                highScore NUMERIC DEFAULT 0,
+                                message TEXT)`)
+    .run(`INSERT INTO users(username, password, highscore, message) VALUES(?, ?, ?, ?)`,
+        ['admin', 'sasageyo', 0, 'The first one']);
+});
+
+function getUsers(callback){
+    db.all('SELECT username, highScore, message FROM users', (err, users) => {
+        if (err){
+            console.error('Error getting usernames');
+        }
+        else{
+            callback(users);
+        }
+    });
+}
+
+function(callback){
+    db.all('SELECT username FROM users', (err, username) => {
+        if (err){
+            console.error('Error getting results');
+        }
+        else{
+            callback(username);
+        }
+    });
+}
+
+function getPasswords(callback){
+    db.all('SELECT password FROM users', (err, password) => {
+        if (err){
+            console.error('Error getting results');
+        }
+        else{
+            callback(password);
+        }
+    });
+}
+
+module.exports.getUsers = getUsers;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ let app = express();
 let session = require('express-session');
 const { v4: uuidv4 } = require('uuid');
 
+const model = require('./public/scripts/databasehandle.js');
+
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(session({
@@ -14,6 +17,11 @@ app.use(session({
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+
+function userExists(user, pass){
+    users = model.getUsernames(user)
+    passwords = 
+};
 
 app.get('/newGame', function(request, response) {
     response.render('newGame', {
@@ -29,17 +37,74 @@ app.get('/login', function(request, response){
     });
 });
 
-// first page
 app.get('/', function(request, response) {
     response.send('Welcome to our page!');
 });
 
-//Test data
+app.post('/processLogin', function(request, response){
+    let username= request.body.username;
+    let password= request.body.password;
 
+    if(userExists(username, password)){
+        request.session.username = username;
+        request.session.password = password;
+
+        response.render('loginConfirmed', {
+            title: 'Login Succesful',
+            username: username
+        });
+    } 
+    else{
+        response.render('login', {
+            title: 'Login Page',
+            errorMessage: 'Invalid Login'
+        })
+    }
+});
+
+app.get('/register', function(request, response){
+    response.render('register', {title:'Register'});
+});
+app.post('/processRegistration', function(request, response){
+    let username = request.body.username;
+    let password = request.body.password;
+    let passwordC = request.body.confirmPassword;
+
+    if (userExists(username)) {
+        response.render('register', {
+            title: 'Register',
+            errorMessage: 'Username in use'
+        });
+      } else {
+        usernames.push(username);
+    
+        request.session.username = username;
+        request.session.password=password;
+
+        response.render('loginConfirmed', {
+            title: 'Login Succesful',
+            username: username
+        });
+      }
+});
+
+app.get('/logout', function(request, response){
+    request.session.username='';
+    request.session.password='';
+    response.redirect('/login')
+})
+
+app.get('/userScores', function(request, response){
+    model.getUsers((users) =>{
+        response.render('scores', {
+            title: 'High Scores',
+            database : users
+        });
+    });
+});
 
 app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), function(){
-    //console.log(`listeining for requests on port 3000`);
     console.log(`Listening on port ${app.get('port')}`);
 });
